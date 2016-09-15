64886c9afebd36acb53a754a0465f9dadd7733be
Mathias Bellerby <Mathis2501@gmail.com>
2016-09-15 11:00:01 +0200

    11 Pass

---
+++ 05_Temperature/ITemperature.cs
@@ -0,0 +1,17 @@
+﻿namespace _05_Temperature
+{
+    internal interface ITemperature
+    {
+    }
+
+    internal class TemperatureFactory
+    {
+        internal static ITemperature Get(Unit u)
+        {
+            
+        }
+        
+    }
+
+    
+}
---
+++ 05_Temperature/Temperature.cs
@@ -0,0 +1,40 @@
+﻿namespace _05_Temperature
+{
+    public enum Unit
+    {
+        Celcius,
+        Fahrenheit
+    }
+    public class Temperature
+    {
+        public Temperature()
+        {
+
+        }
+
+        public Temperature(Unit u, double i)
+        {
+            if (u == Unit.Celcius)
+                Celcius = i;
+            else
+                Fahrenheit = i;
+        }
+
+        public double Fahrenheit;
+        public double Celcius;
+
+        public static double FahrenheitToCelcius(double OrigFahren)
+        {
+            return (OrigFahren - 32) * 5 / 9;
+        }
+
+        public static double CelciusToFahrenheit(double OrigCelc)
+        {
+            return OrigCelc * 9 / 5 +32;
+        }
+
+        
+        
+    }
+}
+
