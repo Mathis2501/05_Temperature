008d217f18ff4cae7760c2fb3d1bcede5324f58f
Mathias Bellerby <Mathis2501@gmail.com>
2016-09-15 11:23:19 +0200

    All tests pass

--- 05_Temperature/ITemperature.cs
+++ 05_Temperature/ITemperature.cs
@@ -1,17 +1,39 @@
-﻿namespace _05_Temperature
+﻿using System;
+
+namespace _05_Temperature
 {
     internal interface ITemperature
     {
+        double Convert(double T);
     }
-
     internal class TemperatureFactory
     {
-        internal static ITemperature Get(Unit u)
+        internal static ITemperature Get(Unit E)
         {
-            
+            if (E == Unit.Celcius)
+            {
+                return new Celcius();
+            }
+            else
+            {
+                return new Fahrenheit();
+            }
         }
-        
+
     }
 
-    
+    internal class Celcius : ITemperature
+    {
+        public double Convert(double T)
+        {
+            return (5 * T - 160) / 9;
+        }
+    }
+    internal class Fahrenheit : ITemperature
+    {
+        public double Convert(double T)
+        {
+            return T * 9 / 5 + 32;
+        }
+    }
 }
